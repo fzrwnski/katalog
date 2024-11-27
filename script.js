6a3d2912c16fbee1257df4b84ea04013951f2fc6
fzrwnski <fzrwn001@gmail.com>
2024-11-27 07:14:45 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.whatsapp-button').forEach(button => {
-    button.addEventListener('click', function(event) {
-        event.preventDefault();
-        const flavor = this.getAttribute('data-flavor');
-        const message = `I want a ${flavor}. Today is ${new Date().toLocaleDateString()}`;
-        const whatsappUrl = `https://wa.me/+62895424461420?text=${encodeURIComponent(message)}`;
-        window.open(whatsappUrl, '_blank');
-    });
-});
